19a0f1396bd116d2a208a21ae2b3a13359223392
Deep Patel <deep.patel9593@gmail.com>
2021-05-24 11:53:03 +0530

    bug fixes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const { program } = require('commander');
 const packageConfig = require('./package.json');
 const { green, red } = require('chalk');
@@ -14,40 +13,49 @@ const { createService } = require('./core/createService/createService');
 program.version(packageConfig.version).description(packageConfig.description);
 
 //clizard init
-program.command('init').description('To initialize the basic setup').action(() => {
-	init()
-		.then((result) => {
-			if (result) {
-				global.isConfigured = false;
-				console.log(green('Setup is ready'));
-			}
-		})
-		.catch((err) => {
-			console.log(red(err));
-		});
-});
+program
+	.command('init')
+	.description('To initialize the basic setup')
+	.action(() => {
+		init()
+			.then((result) => {
+				if (result) {
+					global.isConfigured = false;
+					console.log(green('Setup is ready'));
+				}
+			})
+			.catch((err) => {
+				console.log(red(err));
+			});
+	});
 
 //clizard create-api-module
-program.command('create-api-module <moduleName>').description('to create a api module').action((moduleName) => {
-	createApiModule(moduleName)
-		.then((result) => {
-			if (result) ora('Command ran successfully').succeed();
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-});
+program
+	.command('create-api-module <moduleName>')
+	.description('to create a api module')
+	.action((moduleName) => {
+		createApiModule(moduleName)
+			.then((result) => {
+				if (result) ora('Command ran successfully').succeed();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 
 // clizard create-api
-program.command('create-api').description('to create an api endpoint').action(() => {
-	createApi()
-		.then((result) => {
-			ora('Created successfully').succeed();
-		})
-		.catch((err) => {
-			console.log(err.message);
-		});
-});
+program
+	.command('create-api')
+	.description('to create an api endpoint')
+	.action(() => {
+		createApi()
+			.then((result) => {
+				ora('Created successfully').succeed();
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	});
 
 //to create functions
 program
@@ -64,25 +72,31 @@ program
 	});
 
 //to create global middleware
-program.command('create-middleware').description('To create a global middleware boilerplate').action(() => {
-	createMiddleware()
-		.then((result) => {
-			console.log('Command ran successfully');
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-});
+program
+	.command('create-middleware')
+	.description('To create a global middleware boilerplate')
+	.action(() => {
+		createMiddleware()
+			.then((result) => {
+				console.log('Command ran successfully');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 
 //to create a global service
-program.command('create-service').description('To create a global service boilerplate').action(() => {
-	createService()
-		.then((result) => {
-			console.log('Command ran successfully');
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-});
+program
+	.command('create-service')
+	.description('To create a global service boilerplate')
+	.action(() => {
+		createService()
+			.then((result) => {
+				console.log('Command ran successfully');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 
 program.parse(process.argv);
